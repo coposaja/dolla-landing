312a845fa3e83ec30a12a811e700a949d49fe826
Yohanzen Christanto Alexander <yohanzen.alexander@binus.edu>
2021-01-01 02:14:02 +0700

    Add Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter } from "react-router-dom";
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Sidebar />
     </BrowserRouter>
   );
 }
