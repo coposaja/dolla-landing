3dd8e6e1bea177a993ccdd9f379d89287987638c
Yohanzen Christanto Alexander <yohanzen.alexander@binus.edu>
2021-01-01 17:16:16 +0700

    Add Sidebar toggle functionality

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { FaBars } from 'react-icons/fa';
 import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">dolla</NavLogo>
-          <MobileIcon>
+          <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Icon, SidebarContainer, CloseIcon, SidebarWrapper, SidebarLink, SideBtnWrap, SidebarRoute, SidebarMenu } from './SidebarElements';
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
-    <SidebarContainer>
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
       <Icon>
         <CloseIcon />
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to="about">About</SidebarLink>
-          <SidebarLink to="discover">Discover</SidebarLink>
-          <SidebarLink to="services">Services</SidebarLink>
-          <SidebarLink to="signup">Sign Up</SidebarLink>
+          <SidebarLink onClick={toggle} to="about">About</SidebarLink>
+          <SidebarLink onClick={toggle} to="discover">Discover</SidebarLink>
+          <SidebarLink onClick={toggle} to="services">Services</SidebarLink>
+          <SidebarLink onClick={toggle} to="signup">Sign Up</SidebarLink>
         </SidebarMenu>
         <SideBtnWrap>
           <SidebarRoute to="/signin">Sign In</SidebarRoute>
