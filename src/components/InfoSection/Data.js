e0a42618400460235811cf00071f70279e3ab75b
Yohanzen Christanto Alexander <yohanzen.alexander@binus.edu>
2021-01-01 19:25:28 +0700

    Add About section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+import CarImg from '../../assets/images/car.svg';
+
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headLine: 'Unlimited Transaction with zero fees',
+  description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: CarImg,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+}
